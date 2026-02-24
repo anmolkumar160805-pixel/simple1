6748b2c9702fbc75ffa9e18016d7831919e16761
anmolkumar160805-pixel <anmolkumar160805@gmail.com>
2026-02-24 20:26:42 +0530

    Day 5: Initial project setup

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById("status").innerText = "Welcome back, Builder.";
+}
+
+function updateMessage() {
+    document.getElementById("status").innerText = "Day 3 Completed 🚀";
+}
